dd6d94590e3f5ba490d68cbe83416aba18501fa8
darrin allen <darrin.lovard.allen@gmail.com>
2022-12-10 15:20:19 -0800

    finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,11 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/darrinlallen')
+.then( resp => { 
+  document.querySelector('.cards').appendChild(githubCard(resp.data));
+})
+.catch(err => console.error(err))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -29,7 +33,47 @@
 */
 
 const followersArray = [];
+function githubCard(gitInfo){
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const login = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
 
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  login.classList.add('username')
+
+  img.src = gitInfo.avatar_url;
+  name.textContent = gitInfo.name;
+  login.textContent = gitInfo.login;
+  location.textContent = gitInfo.location;
+  profile.textContent = "Profile"
+  profileLink.textContent = "link to profile"
+  profileLink.href = gitInfo.html_url
+  followers.textContent = gitInfo.followers;
+  following.textContent = gitInfo.following;
+  bio.textContent = gitInfo.bio
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(login);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink)
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  return card;
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
